7eba15bd6bc885c232d440ef5681997dccc2d53d
Hisato Kawaminami and Steven Colburn <steven@>
2018-05-30 11:39:56 -0700

    add jquery to add strings to lists.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+function prependList(stringOne,where,type){
+  var returnString = "";
+  returnString = returnString.concat("<", type, ">", stringOne, "</", type, ">");
+  $(where).prepend(returnString);
+}
+
 $(function(){
+
+  $("button#hello").click(function () {
+    prependList("Hello!","ul#user","li");
+    prependList("Why hello there!","ul#webpage","li");
+  });
+
+  $("button#goodbye").click(function () {
+    prependList("Goodbye!","ul#user","li");
+    prependList("Goodbye, dear user!","ul#webpage","li");
+  });
+
+  $("button#stop").click(function () {
+    prependList("Stop copying me!","ul#user","li");
+    prependList("Pardon me. I meant no offense.","ul#webpage","li");
+  });
 
 });
