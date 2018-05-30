552d79f2ee1f4b80d88809db3aaa7c57285d8da9
Hisato Kawaminami and Steven Colburn <steven@>
2018-05-30 13:16:13 -0700

    add the ability to remove from list when clicking on object in list

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,40 @@ function prependList(stringOne,where,type){
   $(where).prepend(returnString);
 }
 
+function removeOnClick(where,type)
+{
+  $(where).children(type).first().click(function()
+  {
+    $(this).remove();
+  });
+}
+
+function turnColor()
+{
+  $("li").css("background-color","green");
+}
+
 $(function(){
 
   $("button#hello").click(function () {
     prependList("Hello!","ul#user","li");
     prependList("Why hello there!","ul#webpage","li");
+    removeOnClick("ul#user","li");
+    removeOnClick("ul#webpage","li");
   });
 
   $("button#goodbye").click(function () {
     prependList("Goodbye!","ul#user","li");
     prependList("Goodbye, dear user!","ul#webpage","li");
+    removeOnClick("ul#user","li");
+    removeOnClick("ul#webpage","li");
   });
 
   $("button#stop").click(function () {
     prependList("Stop copying me!","ul#user","li");
     prependList("Pardon me. I meant no offense.","ul#webpage","li");
+    removeOnClick("ul#user","li");
+    removeOnClick("ul#webpage","li");
   });
 
 });
